c4c3567537ec529583dae67f28b065af996ad91d
Mikaella Antonette Tayoto <mikaellatayoto04@gmail.com>
2025-04-03 22:23:20 +0800

    Connect user_management to database

--- PHP/dbcon.php
+++ PHP/dbcon.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "Tayotomika04";
 $database = "pup_trackersys";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("❌ Connection failed: " . mysqli_connect_error());
+} else {
+//   echo "✅ Connected to the database!";
 }
-
-//echo "Connected successfully";
 ?>
