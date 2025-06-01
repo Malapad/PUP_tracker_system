36292494f23147c5867869682b9b651174b81c7c
Andreadonatos <dreadonatos@gmail.com>
2025-06-01 17:38:39 +0800

    Delete the old admin violation page and updated linkings

--- updated-admin-violation/admin-violationpage.php
+++ updated-admin-violation/admin-violationpage.php
@@ -420,7 +420,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'get_violation_type_details' &&
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Student Violation Records</title>
+    <title>Admin Sanction Page</title>
     <link rel="stylesheet" href="./admin_violation.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
