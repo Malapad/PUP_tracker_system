460d3157c0d2f0a5cab554bb1b32728547c590b7
Andreadonatos <dreadonatos@gmail.com>
2025-06-10 07:05:34 +0800

    add the security log in

--- index files/index.js
+++ index files/index.js
@@ -1,7 +1,10 @@
-function selectRole(role) {
-  if (role === 'student') {
-    window.location.href = '../student-page/student_login.php';
-  } else if (role === 'admin') {
-    window.location.href = '../admin-login/admin_login.php';
-  }
-}
+const buttonA = document.getElementById("student-btn");
+const buttonB = document.getElementById("admin-btn");
+
+buttonA.addEventListener("click", function() {
+    window.location.href = "../student-page/student_login.php"; 
+});
+
+buttonB.addEventListener("click", function() {
+    window.location.href = "../admin-login/admin_login.php"; 
+});
